11bc151b239e87c93d36caa0344aa27d70098291
Explore <1989397949@qq.com>
2025-05-03 14:37:26 +0800

    [?] add animation [alpha]

--- js/script.js
+++ js/script.js
@@ -2,3 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('loader').style.display = 'none';
 	console.log('页面html已加载完成');
 });
+
+document.fonts.ready.then(() => {
+  console.log('所有字体加载完成');
+  document.documentElement.classList.add('fonts-loaded');
+});
